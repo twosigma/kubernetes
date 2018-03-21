e39122dd46642851b0d3486e6cc9cd89913c1507
Andrew Kochut <kochut@ts01.twosigma.com>
2018-03-20 21:20:06 -0400

    Cherrypick of patch to prevent path overlap for mounts.
    Add validation for overlapping mountpoints
    
    Currently when using overlapping paths, only one of the paths end up in
    the container. Instead of letting the container start in this state,
    provide an error message with the disallowed overlapping paths.

--- pkg/api/validation/validation.go
+++ pkg/api/validation/validation.go
@@ -1936,6 +1936,18 @@ func ValidateVolumeMounts(mounts []api.VolumeMount, volumes sets.String, contain
 		if mountpoints.Has(mnt.MountPath) {
 			allErrs = append(allErrs, field.Invalid(idxPath.Child("mountPath"), mnt.MountPath, "must be unique"))
 		}
+		// add check for path overlap
+		for _, apath := range mountpoints.List() {
+			rel, err := filepath.Rel(mnt.MountPath, apath)
+			if err != nil {
+				// returns error if target path can't be made relative to basepath, which means no conflict
+				continue
+			}
+			if !strings.Contains(rel, "../") {
+				errorString := fmt.Sprintf("conflicts with volumeMount:'%v', volumeMounts may not overlap one another", apath)
+				allErrs = append(allErrs, field.Invalid(idxPath.Child("mountPath"), mnt.MountPath, errorString))
+			}
+		}
 		mountpoints.Insert(mnt.MountPath)
 		if len(mnt.SubPath) > 0 {
 			allErrs = append(allErrs, validateLocalDescendingPath(mnt.SubPath, fldPath.Child("subPath"))...)
